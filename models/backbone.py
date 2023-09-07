e1053ba9c2f56539df662e1e4db1b36fb7980f23
QianJoe <97387470@qq.com>
2023-09-07 11:42:28 +0800

    Add files via upload
    TALDS-Net Code

---
+++ models/backbone.py
@@ -0,0 +1,112 @@
+import sys
+
+from torch import nn
+
+sys.dont_write_bytecode = True
+
+
+class CNNEncoder(nn.Module):
+    def __init__(self, in_c=3):
+        super(CNNEncoder, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_c, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64, affine=True),
+            nn.LeakyReLU(0.2, True),
+            nn.MaxPool2d(kernel_size=2, stride=2), )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64, affine=True),
+            nn.LeakyReLU(0.2, True),
+            nn.MaxPool2d(kernel_size=2, stride=2), )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64, affine=True),
+            nn.LeakyReLU(0.2, True), )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64, affine=True),
+            nn.LeakyReLU(0.2, True), )
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        return out
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.LeakyReLU(0.2, inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.LeakyReLU(0.2, inplace=True)
+        self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+        self.relu3 = nn.LeakyReLU(0.2, inplace=True)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.downsample = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels),
+            )
+
+    def forward(self, x):
+        identity = self.downsample(x)
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu2(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out = self.relu3(out)
+
+        out += identity
+        out = self.pool(out)
+
+        return out
+
+
+class ResNet12Encoder(nn.Module):
+    def __init__(self, num_classes=100):
+        super(ResNet12Encoder, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.LeakyReLU(0.1, inplace=True)
+        self.layer1 = self.make_layer(BasicBlock, 64, 2, stride=1)
+        self.layer2 = self.make_layer(BasicBlock, 128, 2, stride=2)
+        self.layer3 = self.make_layer(BasicBlock, 256, 2, stride=2)
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(256, num_classes)
+
+    def make_layer(self, block, out_channels, blocks, stride):
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels
+        for _ in range(1, blocks):
+            layers.append(block(out_channels, out_channels, stride=1))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
